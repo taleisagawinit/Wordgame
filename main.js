393eeef79ebf1083390966629b8cf8f644bcd524
PunchCode <punchcode@PunchCodes-MacBook-Pro.local>
2019-07-17 11:45:44 -0700

    added nightmare mode

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 $(document).ready(() => {
 
-
     var wordlist = commonWords.filter(function(a) {
         if (a.length > 3) {
             return true;
@@ -37,10 +36,68 @@ $(document).ready(() => {
     var wordArray = yourWord.split('')
     // var wrongGuess = "";
     var allGuesses = "";
-    
-    function addLetters(letter) {
-        // fix guesses so they know AALLL letters guessed. not just wrong ones
+    var health = 500;
 
+    //maybe make a constructor function and make all these sounds methods:
+
+    function playWin() {
+        var snd = new Audio("https://freesound.org/data/previews/267/267528_5060556-lq.mp3");
+        snd.play();
+        snd.currentTime=0;
+    }
+    
+    function playLose() {
+        var losesnd = new Audio("https://freesound.org/data/previews/157/157218_1670580-lq.mp3");
+        losesnd.play();
+        losesnd.currentTime=0;
+    }
+
+    function lostGame() {
+        var losegm = new Audio("https://freesound.org/data/previews/362/362205_6629901-lq.mp3");
+        losegm.play();
+        losegm.currentTime=0;
+    }
+
+    function wonGame() {
+        var losegm = new Audio("https://freesound.org/data/previews/270/270319_5123851-lq.mp3");
+        losegm.play();
+        losegm.currentTime=0;
+    }
+
+
+
+    function wrong() {
+        // alert('Wrong Guess')
+        // wrongGuess = wrongGuess + $("#guess").val()
+        allGuesses = allGuesses + $("#guess").val();
+        $("#guess").val("")
+        guessesLeft--;
+        $(".allGuesses").html(allGuesses);
+        health = health - 50;
+        $("#health").css("width",`${health}px`);
+        if (guessesLeft == 0) {
+            alert('You lost! Your word was ' + "'" + yourWord + "'")
+            return lostGame();
+        } 
+        return playLose();
+        
+    }
+
+    function right() {
+        if (spacesArray.indexOf("_") == -1) {
+            alert('You won the game!')
+            return wonGame();
+        }
+        playWin();
+        // alert('Correct Guess')
+        allGuesses = allGuesses + $("#guess").val();
+        $("#guess").val("")
+        $(".allGuesses").html(allGuesses);
+    }
+    
+    
+
+    function addLetters(letter) {
         for (var i = 0; i <= wordArray.length-1; i++) {
                if (wordArray[i] === letter) {
                 spacesArray[i] = letter;
@@ -51,19 +108,11 @@ $(document).ready(() => {
                             spacesArray;
                         }
                     }
-                    alert('Correct Guess')
-                    allGuesses = allGuesses + $("#guess").val();
-                    $("#guess").val("")
-                    $(".allGuesses").html(allGuesses);
+                    right();
                     return $(".word").html(spacesArray.join(''))
                 }
         }
-        alert('Wrong Guess')
-        // wrongGuess = wrongGuess + $("#guess").val()
-        allGuesses = allGuesses + $("#guess").val();
-        $("#guess").val("")
-        guessesLeft--;
-        $(".allGuesses").html(allGuesses);
+        wrong()
         return $(".num").html(guessesLeft);
     }
 
@@ -72,15 +121,11 @@ $(document).ready(() => {
         // e.preventDefault();
         if (allGuesses.charAt(allGuesses.length-1) === $("#guess").val()) {
             $("#guess").val("")
-            // remove return value below if I want to still return that the letter is wrong and minus guesses left or right
+            // remove return value below if I want to return that the letter is wrong AND minus guesses left
             return alert('You already guessed this letter! Try a different one')
         }
         addLetters($("#guess").val())
-        if (guessesLeft == 0) {
-            alert('You lost! Your word was ' + "'" + yourWord + "'")
-        } else if (spacesArray.indexOf("_") == -1) {
-            alert('You won the game!')
-        }
+
     })
     
 
